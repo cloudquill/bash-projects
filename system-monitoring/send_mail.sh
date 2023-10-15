62c55e44fa0e1b542e6201c177482fcd1a2b808d
cloudquill <ifeanyiojukwu11@outlook.com>
2023-10-15 14:45:25 +0100

    Simplified the email-sending feature to use just the ssmtp utility Improved error reporting to notify admins via email when a script fails Expanded comments to include more information on what some parts of the script do Fixed an issue where the script couldn't send email alerts when loaded in a cron job

--- system-monitoring/send_mail.sh
+++ system-monitoring/send_mail.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 #
-# This script sends an email using provided parameters. The script is sourced 
-# in the system_monitoring.sh script so it can use the forward_email() 
-# function.
+# This script sends an email using provided argument which would be an email 
+# text containing the email headers and message. The ssmtp utility with the 
+# option -t tells it to read the input text for the headers and message. These 
+# headers begin with keywords such as 'To', 'From' and 'Subject'. 
+#
+# Ensure to leave a space after defining the headers to begin the email body.
+#
+# So it seems cron cannot run the ssmtp command unless we include the full path 
+# to the ssmtp binary. This is because cron operates in a limited environment.
+#
+# Apparently, the current usage of ssmtp below requires the use of the here 
+# document (EOF)
 
 forward_email(){
-recipient=$1
-subject=$2
-body=$3
 
-echo -e "From: cloudquill\nTo: $recipient\nSubject: $subject\n\n$body" | sendmail -t
+  /usr/sbin/ssmtp -t << EOF
+  ${1}
+EOF
 }
--- system-monitoring/system_monitoring.sh
+++ system-monitoring/system_monitoring.sh
@@ -2,30 +2,44 @@
 #
 # This script monitors and logs the CPU and Memory usage of a local system. It
 # also sends out email alerts if one or more of the monitored metric is above
-# a threshold
+# a threshold.
 
-# Source send_mail script to use the forward_mail() function
+# Source send_mail script to use the forward_mail() function. More information
+# on how this works is in the send_mail script.
 source send_mail.sh
+
+# Store the name of this script for error reporting.
+filename=$(basename "$0")
 
 # Threshold values for CPU and Memory usage to trigger alerts
 cpu_threshold=1
 memory_threshold=20
 
-# Capture system information and exit status in case of errors
+# Capture system information and exit status in case of errors.
+#
+# The 12th field of the command 'mpstat' list the percentage of cpu that is
+# idle. Using the awk command with regex to extract the decimal part and
+# subtracting from 100 gives us the percentage that is being used.
 user_cpu_usage=$(mpstat | awk '$12 ~ /[0-9.]+/ { print 100 - $12 }')
 mpstat_command_exit_status=$?
 
+# The free command lists the hard disk and RAM information such as the total
+# or used memory. Then we calculate the percentage of used hard disk space 
+# with the second and third fields in the second row.
 user_memory_usage=$(free --mega | awk 'NR==2{print $3*100/$2}')
 free_command_exit_status=$?
 
-# Check if the 'mpstat' and 'free' commands executed successfully
+# Check if the 'mpstat' and 'free' commands executed successfully.
+# Otherwise send an error message to an admin to notify them.
 if [[ $mpstat_command_exit_status -ne 0 ]]; then
-  echo "Error: Failed to execute 'mpstat' command."
+  email_txt="An error occurred when running the 'mpstat' command for ${filename} file. Please review!"
+  forward_email "${email_txt}"
   exit 1
 fi
 
 if [[ $free_command_exit_status -ne 0 ]]; then
-  echo "Error: Failed to execute 'free' command."
+  email_txt="An error occurred when running the 'free' command for ${filename} file. Please review!"
+  forward_email "${email_txt}"
   exit 1
 fi
 
@@ -34,21 +48,23 @@ echo -e "\nSystem information as at $(date)\nCPU Usage: ${user_cpu_usage}% \nMem
 
 # Send an email if at least one metric is above the threshold
 if [[ $user_cpu_usage > $cpu_threshold ]] || [[ $user_memory_usage > $memory_threshold ]]; then
-  recipient="ifeanyiojukwu11@outlook.com"
-  subject="System Monitoring Alert"
 
   # I ignored the indentation to eliminate unnecessary white spaces in the
   # email body.
-  body="
+  email_txt="
+To: ifeanyiojukwu11@outlook.com
+From: $(hostname)
+Subject: System Monitoring Alert
+X-Custom-Header: Custom value
+
 System Information:
 
-Hostname: $(hostname)
 CPU Usage: ${user_cpu_usage}%
 Memory Usage: ${user_memory_usage}%
 
 One or more of these metrics are above the threshold value. Please review!"
   
-  # Call email script
-  forward_email "${recipient}" "${subject}" "${body}"
+  # Call the forward_email() function with email_txt as an argument.
+  forward_email "${email_txt}"
 
 fi
